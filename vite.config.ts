ffa1d26d35b2738317e272fa1b07da9bf90031d9
yinkouxi <3405176636@qq.com>
2023-12-07 20:44:19 +0800

    vite配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 设置文件别名./src路径为@
+  resolve: {
+    // 设置文件./src路径为 @
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, './src')
+      }
+    ]
+  }
 })
