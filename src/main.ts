9d4f819ae22f9e3dc9f18fbee30f03b450f11603
yinkouxi <3405176636@qq.com>
2023-12-10 15:41:07 +0800

    配置eslint-prettier约束代码风格

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,12 @@ import App from '@/App.vue'
 
 createApp(App).mount('#app')
 console.log(import.meta.env.VITE_APP_WEB_URL)
+
+const info = {
+  name: 'Yier',
+  age: 19,
+  school: 'jxa',
+  gota: 'pqx'
+}
+
+console.log(info)
