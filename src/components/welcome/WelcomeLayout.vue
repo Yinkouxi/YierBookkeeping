356f4f473bab779c819021763bcf547987c32a96
yinkouxi <3405176636@qq.com>
2023-12-15 10:42:44 +0800

    重构welcome

--- src/components/welcome/WelcomeLayout.vue
+++ src/components/welcome/WelcomeLayout.vue
@@ -1,19 +1,13 @@
 <template>
-  <div class="wrapper">
-    <h2 class="pass-welcome">跳过</h2>
-    <div class="logo">
-      <img src="@/assets/img/welcome/logo.svg" alt="" />
+  <div class="content">
+    <div class="wel-img">
+      <img :src="welImg" alt="" />
     </div>
-    <div class="content">
-      <div class="wel-img">
-        <img :src="welImg" alt="" />
-      </div>
-      <div class="slogan">
-        <h1>{{ slogan1 }}</h1>
-        <h1>{{ slogan2 }}</h1>
-      </div>
-      <router-link class="next-page" :to="nextPage">{{nextText}}</router-link>
+    <div class="slogan">
+      <h1>{{ slogan1 }}</h1>
+      <h1>{{ slogan2 }}</h1>
     </div>
+    <router-link class="next-page" :to="nextPage">{{ nextText }}</router-link>
   </div>
 </template>
 
@@ -31,61 +25,41 @@ defineProps({
     type: [String, Object] as unknown as () => RouteLocationRaw,
     default: '/'
   },
-  nextText:{
-    type:String,
-    default:'下一页'
+  nextText: {
+    type: String,
+    default: '下一页'
   }
 })
 </script>
 
 <style lang="less" scoped>
-.wrapper {
-  background-color: aquamarine;
-  background-image: linear-gradient(to bottom, #633cc0 0%, #975ad9 100%);
+.content {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 16px 16px 60px 12px;
-  height: 100vh;
+  flex-grow: 1;
+  background-color: #fff;
+  border-radius: 12px;
+  width: 100%;
 
-  .pass-welcome {
-    align-self: flex-end;
-    font-size: 32px;
-    font-weight: 700;
-    color: #fff;
-  }
-  .logo {
+  .wel-img {
     margin-top: 60px;
-    margin-bottom: 20px;
+    flex-grow: 1;
   }
-  .content {
+  .slogan {
     display: flex;
     flex-direction: column;
-    align-items: center;
     flex-grow: 1;
-    background-color: #fff;
-    border-radius: 12px;
-    width: 100%;
-
-    .wel-img {
-      margin-top: 60px;
-      flex-grow: 1;
-    }
-    .slogan {
-      display: flex;
-      flex-direction: column;
-      flex-grow: 1;
-      align-items: center;
-      font-size: 32px;
-      font-weight: 350;
-      color: #000000;
-    }
-    .next-page {
-      flex-grow: 1;
-      color: #6035bf;
-      font-weight: 700;
-      font-size: 32px;
-    }
+    align-items: center;
+    font-size: 32px;
+    font-weight: 350;
+    color: #000000;
+  }
+  .next-page {
+    flex-grow: 1;
+    color: #6035bf;
+    font-weight: 700;
+    font-size: 32px;
   }
 }
 </style>
--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="welcome-page">
-    <router-view></router-view>
+    <router-link class="pass-welcome" to="/start">跳过</router-link>
+    <div class="logo">
+      <img src="@/assets/img/welcome/logo.svg" alt="" />
+    </div>
+    <router-view class="page"></router-view>
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style lang="less" scoped>
-  // .welcome-page{
-  //   display: flex;
-  //   height: 100vh;
-  //   flex-direction: column;
-  //   background-color: #6035BF;
-  // }
+.welcome-page {
+  background-image: linear-gradient(to bottom, #633cc0 0%, #975ad9 100%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px 16px 60px 12px;
+  height: 100vh;
+  width: 100vw;
+
+  .pass-welcome {
+    align-self: flex-end;
+    font-size: 32px;
+    font-weight: 700;
+    color: #fff;
+  }
+  .logo {
+    margin-top: 60px;
+    margin-bottom: 20px;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-grow: 1;
+    background-color: #fff;
+    border-radius: 12px;
+    width: 100%;
+  }
+
+  .page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-grow: 1;
+    background-color: #fff;
+    border-radius: 12px;
+    width: 100%;
+  }
+}
 </style>
