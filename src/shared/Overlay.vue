7ff01be53bb55238bfad7e403a50f19f15d832c8
yinkouxi <3405176636@qq.com>
2023-12-20 01:01:23 +0800

    完善Overlay组件

---
+++ src/shared/Overlay.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <div class="mask" @click="$emit('closeOverlay')"></div>
+    <div class="overlay">
+      <section>
+        <h2 class="user-status">未登录用户</h2>
+        <p class="sing-in">点击这里登录</p>
+      </section>
+      <nav class="side-navbar">
+        <ul>
+          <li>
+            <router-link to="/statistics" class="action">
+              <svgIcon name="chart" width="32px" height="32px"></svgIcon>
+              <span>统计图表</span>
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/export" class="action">
+              <svgIcon name="export" width="32px" height="32px"></svgIcon>
+              <span>导出数据</span>
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/notify" class="action">
+              <svgIcon name="notice" width="32px" height="32px"></svgIcon>
+              <span>记账提醒</span>
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/signout" class="action">
+              <svgIcon name="signOut" width="32px" height="32px"></svgIcon>
+              <span>退出登录</span>
+            </router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+defineProps({})
+const close = () => {}
+</script>
+
+<style lang="less" scoped>
+.mask {
+  position: fixed;
+  z-index: @z-index-overlay;
+  background-color: @overlay-mask-bg;
+  top: @top-safe-area-height;
+  left: 0;
+  width: 100%;
+  height: calc(100% - @top-safe-area-height);
+}
+.overlay {
+  position: fixed;
+  background-color: @overlay-bg;
+  z-index: @z-index-overlay + 1;
+  top: @top-safe-area-height;
+  left: 0;
+  width: 16rem;
+  height: calc(100% - @top-safe-area-height);
+
+  section {
+    background-color: @overlay-section-color;
+    height: 128px;
+    padding: 32px 0 44px 16px;
+    .user-status {
+      color: @overlay-user-status-text-color;
+      font-weight: 350;
+      font-size: 24px;
+    }
+    .sing-in {
+      color: @overlay-sign-in-text-color;
+      margin-top: 12px;
+    }
+  }
+
+  .side-navbar {
+    padding: 18px 0 0 16px;
+    font-size: 20px;
+
+    .action {
+      color: @overlay-side-navbar-text-color;
+      display: flex;
+      align-items: center;
+      margin-top: 12px;
+
+      span{
+        margin-left: 16px;
+      }
+    }
+  }
+}
+</style>
--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -2,7 +2,7 @@
   <div class="start-page">
     <Navbar>
       <template #icon>
-        <svgIcon name="menu" color="white" width="26px" height="26px"></svgIcon>
+        <svgIcon name="menu" color="white" width="26px" height="26px" @click=onClickMenu()></svgIcon>
       </template>
       <template #title>
         <span>山竹记账</span>
@@ -12,9 +12,10 @@
       <svg-icon name="pig" width="128px" height="130px"></svg-icon>
     </CenterContainer>
     <div class="btn-wrapper">
-      <Button class="btn" @click="btnClick">开始记账</Button>
+      <Button class="btn">开始记账</Button>
     </div>
     <FloatButton></FloatButton>
+    <Overlay v-show="overlayVisible" @close-overlay="closeOverlay"></Overlay>
   </div>
 </template>
 
@@ -23,9 +24,17 @@ import Button from '@/shared/Button.vue'
 import FloatButton from '@/shared/FloatButton.vue'
 import CenterContainer from '@/shared/CenterContainer.vue'
 import Navbar from '@/shared/Navbar.vue'
+import Overlay from '@/shared/Overlay.vue'
+import { ref } from 'vue'
 
-const btnClick = () => {
-  console.log('btn click')
+const overlayVisible = ref(false)
+
+const onClickMenu = ()=>{
+  overlayVisible.value= !overlayVisible.value
+}
+
+const closeOverlay =()=>{
+  overlayVisible.value=false
 }
 </script>
 
