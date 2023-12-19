429a9e9526d2256f2b3378e4ca7f767f0c206b8a
yinkouxi <3405176636@qq.com>
2023-12-19 09:14:28 +0800

    FloatButton组件

--- src/shared/FloatButton.vue
+++ src/shared/FloatButton.vue
@@ -1,14 +1,23 @@
 <template>
   <div class="float-button">
-    <svg-icon name="add" color="pink" width="20px" height="20px"></svg-icon>
+    <svg-icon name="add" color="white" width="24px" height="24px"></svg-icon>
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style lang="less" scoped>
 .float-button {
-  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  background-color: @float-button-bg;
+  color: rgb(146, 79, 79);
 }
 </style>
