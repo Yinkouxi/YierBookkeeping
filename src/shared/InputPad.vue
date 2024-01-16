ed33dcfceb60fe81e4f3f1d78b6ecbf9fd93b084
yinkouxi <3405176636@qq.com>
2024-01-16 20:39:40 +0800

    完成datePicker

--- src/shared/InputPad.vue
+++ src/shared/InputPad.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="input-pad">
-    <!-- <h2>input-pad</h2> -->
-    <div class="data-amount">
-      <div class="data">
-        <svgIcon name="data" color="#5c33be" width="26px" height="26px"></svgIcon>
-        <span>{{ time(now).format() }}</span>
+    <div class="date-amount">
+      <div class="date">
+        <svgIcon name="date" color="#5c33be" width="26px" height="26px"></svgIcon>
+        <span>
+          <span @click="showDatePicker">{{ currentDate.join('-') }}</span>
+          <van-popup position="bottom" v-model:show="showPop" :style="{ height: '40%' }">
+            <van-date-picker
+              v-model="currentDate"
+              title="选择日期"
+              @cancel="cancelDatePicker"
+              @confirm="setDate"
+            />
+          </van-popup>
+        </span>
       </div>
       <span class="amount">{{ amount }}</span>
     </div>
     <div class="key-board">
-      <button v-for="(btn,id) in buttons" @click="buttons[id].onclick">{{ btn.text }}</button>
+      <button v-for="(btn, id) in buttons" @click="buttons[id].onclick">{{ btn.text }}</button>
     </div>
   </div>
 </template>
@@ -20,6 +29,25 @@ import { time } from '../shared/time'
 
 const amount = ref('00000')
 const now = new Date()
+const currentDate = ref(time(now).format().split('-'))
+const showPop = ref(false)
+
+const showDatePicker = () => {
+  showPop.value = true
+}
+const hideDatePicker = () => {
+  showPop.value = false
+}
+const cancelDatePicker = () => {
+  //点击取消更新为当天时间  
+  currentDate.value=time(now).format().split('-')
+  hideDatePicker()
+}
+const setDate = (datePic: any) => {
+  currentDate.value = datePic.selectedValues
+  hideDatePicker()
+}
+
 const buttons = [
   {
     text: '1',
@@ -27,9 +55,12 @@ const buttons = [
       console.log('1')
     }
   },
-  { text: '2', onclick: () => {
-    console.log(2)
-  } },
+  {
+    text: '2',
+    onclick: () => {
+      console.log(2)
+    }
+  },
   { text: '3', onclick: () => {} },
   { text: '4', onclick: () => {} },
   { text: '5', onclick: () => {} },
@@ -42,21 +73,18 @@ const buttons = [
   { text: '清空', onclick: () => {} },
   { text: '提交', onclick: () => {} }
 ]
-
-
 </script>
 
 <style lang="less" scoped>
 .input-pad {
   color: blueviolet;
-  .data-amount {
+  .date-amount {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 8px 16px;
     font-family: monospace;
-
-    .data {
+    .date {
       display: flex;
       justify-content: start;
       align-items: center;
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,16 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Components({
+      resolvers: [VantResolver()],
+    }),
     createSvgIconsPlugin({
       // 指定需要缓存的图标文件夹
       iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
